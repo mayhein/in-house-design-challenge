b53c4d4cb4c26e94325f37634173816465cfae3d
Rapi Castillo <racastillo@cto.nyc.gov>
2021-03-26 13:16:23 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <div>
+          <label>Username</label>
+          <input type='text'></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type='password'></input>
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
     </div>
   );
 }
